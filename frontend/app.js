a167db6ab3d1c6f5492acb9540fe4696958c2b35
GMplayer210 <65106745+GMplayer210@users.noreply.github.com>
2025-12-23 08:58:26 +0400

    Added startSession which should start a new game in state 0 (waiting for players)

--- frontend/app.js
+++ frontend/app.js
@@ -31,9 +31,21 @@ let admins = [];
 //List of current games by lobby code
 let games = [];
 //Map of currently ongoing games to admins
-let gamesToAdmins = new Map();
+let gameToAdmin = new Map();
+//Map of admins to games 
+let adminToGame = new Map();
 //Map of currently ongoing games to players (includes admins)
-let gamesToPlayers = new Map();
+let gameToPlayers = new Map();
+//Map of players to games
+let playerToGame = new Map();
+//State of the games can be either
+//  0     - waiting for players
+//  1     - guessing (display image, wait until timer runs out while clients send guesses to redis)
+//  2     - image results (also update total scores)
+//  3     - leaderboard (end of game)
+//states 1 and 2 repeat until all the images have been guessed
+//Map of games to states
+let gameToState = new Map();
 
 //Map of usernames to sockets
 let playersToSockets = new Map();
@@ -48,9 +60,57 @@ function startServer() {
     });
 }
 
+//Update all clients
+function updateAll(game){
+    for (let [player] of gameToPlayers.get(game)){
+        updateClient(player)
+    }
+}
+//Update one client
+function updateClient(player){
+    const socket = playersToSockets.get(player);
+    const playerState = players.get(player);
+    const game = playerToGame.get(player);
+    const gameState = gameToState.get(game);
+    const data = {state: gameState, me: playerState, players: gameToPlayers.get(game)}
+
+    socket.emit('update', data);
+}
+
+//Start a session
+//Generates a random lobby code
+//Adds a new game at state 0
+//Adds the admin (given as parameter)
+function startSession(socket, admin, adminPicture) {
+    let admin_state = {name : admin, picture: adminPicture, current_score: 0};
+    players.set(admin, admin_state);
+    admins.push(admin);
+    playersToSockets.set(admin, socket);
+    socketsToPlayers.set(socket, admin);
+
+    let lobbyCode = -1;
+    do {
+        lobbyCode = Math.floor(Math.random() * 9999);
+    } while (games.includes(lobbyCode));
+
+    games.push(lobbyCode);
+    gameToAdmin.set(lobbyCode, admin);
+    gameToPlayers.set(lobbyCode, [admin]);
+    gameToState.set(lobbyCode, 0);
+    adminToGame.set(admin, lobbyCode);
+    playerToGame.set(admin, lobbyCode);
+
+    updateAll(lobbyCode);
+}
+
 //Handle new connection
 io.on('connection', socket => { 
   	console.log('New connection');
+    
+    //Handle admin starting a new session
+    socket.on('start', (username, picture) => {
+        startSession(socket, username, picture);
+    })
 });
 
 //Start server
