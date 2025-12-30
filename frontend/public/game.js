bfd9af7e89c336340ae61df7abce4d141627fe9a
Ethan <ethan@shillitos.org.uk>
2025-12-30 11:16:27 +0000

    added google maps to webpage

--- frontend/public/game.js
+++ frontend/public/game.js
@@ -1,11 +1,14 @@
 var socket = null;
 
+let map = null;
+
 //Prepare game
 var app = new Vue({
     el: '#game',
     data: {
         connected: false,
-        state: {state: {currentClientMode: 0, gameState: 3}, isAdmin: true, player: {name: "me", currentScore: 50}, otherPlayers: [{name: "1", currentScore: 20},{name: "2", currentScore: 60},{name: "3", currentScore: 40},{name: "4", currentScore: 40}]},
+        //state: {state: {currentClientMode: 2, gameState: 1}, isAdmin: true, player: {name: "me", currentScore: 50, guess: null}, otherPlayers: [{name: "1", currentScore: 20, guess: null},{name: "2", currentScore: 60, guess: null},{name: "3", currentScore: 40, guess: null},{name: "4", currentScore: 40, guess: null}]},
+        state: {state: {currentClientMode: 2, gameState: 2}, isAdmin: true, player: {name: "me", currentScore: 50, guess: {lat: 50.923406, lng: -1.401113}}, otherPlayers: [{name: "1", currentScore: 20, guess: {lat: 50.92341, lng: -1.401113}},{name: "2", currentScore: 60, guess: {lat: 50.92541, lng: -1.405113}},{name: "3", currentScore: 40, guess: {lat: 50.92341, lng: -1.411113}},{name: "4", currentScore: 40, guess: {lat: 50.93341, lng: -1.401193}}]},
         leaderboard: [{name: "placeholder", currentScore: 0}],
         registering: false,
         username: "",
@@ -13,6 +16,8 @@ var app = new Vue({
         confirmPassword: "",
         code: "12345",
         errorMessage: "",
+        //awnserPos: null,
+        awnserPos: {lat: 50.923406, lng: -1.401113},
         /*
         messages: [],
         chatmessage: '',
@@ -54,12 +59,12 @@ var app = new Vue({
         this.errorMessage = message;
        },
        login() {
-        socket.emmit('login', [this.username,this.password]);
+        socket.emit('login', [this.username,this.password]);
         this.password = "";
        },
        register() {
         if (this.password == this.confirmPassword) {
-            socket.emmit('register', [this.username,this.password]);
+            socket.emit('register', [this.username,this.password]);
             this.password = "";
             this.confirmPassword = "";
         }
@@ -76,13 +81,13 @@ var app = new Vue({
         this.registering = false;
        },
        startLobby() {
-        socket.emmit('start')
+        socket.emit('start')
        },
        joinLobby() {
-        socket.emmit('join', code)
+        socket.emit('join', code)
        },
        advance() {
-        socket.emmit('advance');
+        socket.emit('advance');
        },
        update(state) {
         this.state = state
@@ -124,4 +129,60 @@ function connect() {
         app.handleChat(message);
     });
     */
-}
+};
+
+//handle maps
+function mapClicked(latLng) {
+    if (app.state.player.guess == null) {
+        map.panTo(latLng);
+        app.state.player.guess = latLng;
+        new google.maps.marker.AdvancedMarkerElement({
+            map,
+            position: latLng,
+        });
+        socket.emit("guess", latLng);
+    }
+};
+
+function initMap() {
+    if (document.getElementById('map') != null) {
+        let soton = {lat: 50.923406, lng: -1.401113}
+        map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 12, center: soton, mapId: "MAP_ID"}
+        );
+
+        //add guess markers
+        if (app.state.player.guess != null) {
+            new google.maps.marker.AdvancedMarkerElement({
+                map,
+                position: app.state.player.guess,
+            });
+        }
+        
+        for (const player of app.state.otherPlayers) {
+            if (player.guess != null) {
+                new google.maps.marker.AdvancedMarkerElement({
+                    map,
+                    position: player.guess,
+                    //try to add image if possible
+                });
+            }
+        }
+
+        //if (app.awnserPos != null) {
+        //    new google.maps.marker.AdvancedMarkerElement({
+        //            map,
+        //            position: app.awnserPos,
+        //        });
+        //}
+
+        if (app.state.state.gameState == 1) {
+            map.addListener("click", (e) => {
+                mapClicked(e.latLng);
+            });
+        }
+        return
+    }
+    setTimeout(initMap, 1000);
+};
+        
