6db1336f094669d506867e27bd129d8b016f9165
AInan05 <147404730+AInan05@users.noreply.github.com>
2026-01-06 14:56:20 +0000

    Update function_app.py
    small fix

--- backend/func_app/function_app.py
+++ backend/func_app/function_app.py
@@ -558,8 +558,6 @@ def join_game(req: func.HttpRequest) -> func.HttpResponse:
         logging.exception("Error in join_game")
         return _json({"result": False, "msg": str(e)}, 500)
 
-"""
-
 # Quit game
 # Removes player from the lobby
 @app.route(route="quit_game", auth_level=func.AuthLevel.FUNCTION, methods=["POST"])
@@ -602,7 +600,6 @@ def quit_game(req: func.HttpRequest) -> func.HttpResponse:
 
 # Change settings
 # Takes new settings and changes it in the database
-@app.route(route="change_settings", auth_level=func.AuthLevel.FUNCTION, methods=["PUT"])
 @app.route(route="change_settings", auth_level=func.AuthLevel.FUNCTION, methods=["PUT"])
 def settings(req: func.HttpRequest) -> func.HttpResponse:
     # expects: {matchCode: code, matchSettings:{noOfRounds:int, maxPlayers:int, countdown:int}}
